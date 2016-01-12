726957233dc5a67a00bf8ddbe43866473d0bb855
303153041@qq.com <fengxiang1990529>
2016-01-12 18:33:25 +0800

    修改包名

--- app/src/main/java/app/fxa/com/appframework/activity/LoginActivity.java
+++
@@ -1,9 +0,0 @@
-package app.fxa.com.appframework.activity;
-
-import app.fxa.com.appframework.BaseActivity;
-
-/**
- * Created by admin on 2016/1/12.
- */
-public class LoginActivity extends BaseActivity {
-}
--- app/src/main/java/app/fxa/com/appframework/util/RequestURL.java
+++
@@ -1,6 +0,0 @@
-package app.fxa.com.appframework.util;
-
-public class RequestURL {
-
-
-}
