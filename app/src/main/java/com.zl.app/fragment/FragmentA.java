b8028ac646bce1fe04dbd31aa98e9a4113e56247
303153041@qq.com <fengxiang1990529>
2016-02-16 18:09:16 +0800

    首页和新闻接口 部分实现

--- app/src/main/java/com.zl.app/fragment/FragmentA.java
+++ app/src/main/java/com.zl.app/fragment/FragmentA.java
@@ -1,19 +1,16 @@
 package com.zl.app.fragment;
 
-import android.support.annotation.NonNull;
-import android.text.InputType;
-import android.text.method.PasswordTransformationMethod;
+import android.content.Context;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.android.volley.VolleyError;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.android.volley.toolbox.NetworkImageView;
+import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
+import com.bigkoo.convenientbanner.holder.Holder;
 import com.zl.app.BaseFragment;
 import com.zl.app.R;
 import com.zl.app.data.home.HomeService;
@@ -22,6 +19,7 @@ import com.zl.app.data.home.model.Ad;
 import com.zl.app.data.home.model.News;
 import com.zl.app.util.AppConfig;
 import com.zl.app.util.AppManager;
+import com.zl.app.util.RequestURL;
 import com.zl.app.util.net.BaseResponse;
 import com.zl.app.util.net.DefaultResponseListener;
 
@@ -29,10 +27,7 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 /**
@@ -46,21 +41,33 @@ public class FragmentA extends BaseFragment {
     @ViewById(R.id.container)
     LinearLayout linearLayout;
 
+    ConvenientBanner convenientBanner;
     List<View> views;
     int pageNo = 1;
     int pageSize = 10;
 
     HomeService homeService;
+
     @AfterViews
     void afterViews() {
+        convenientBanner = (ConvenientBanner) getView().findViewById(R.id.convenientBanner);
         homeService = new HomeServiceImpl();
         homeService.getHomeAds(AppConfig.getUid(AppManager.getPreferences()), new DefaultResponseListener<BaseResponse<List<Ad>>>() {
             @Override
             public void onSuccess(BaseResponse<List<Ad>> response) {
-                Log.e(TAG,response.toString());
+                Log.e(TAG, response.toString());
                 List<Ad> ads = response.getResult();
-                for(Ad ad:ads){
-                    Log.e(TAG,ad.toString());
+                Ad ad = ads.get(0);
+                ads.add(ad);
+                convenientBanner.setPages(
+                        new CBViewHolderCreator<LocalImageHolderView>() {
+                            @Override
+                            public LocalImageHolderView createHolder() {
+                                return new LocalImageHolderView();
+                            }
+                        }, ads);
+                if (ads.size() <= 1) {
+                    convenientBanner.setManualPageable(false);
                 }
             }
 
@@ -70,14 +77,14 @@ public class FragmentA extends BaseFragment {
             }
         });
 
-        homeService.getHomeNews(AppConfig.getUid(AppManager.getPreferences()),pageNo,pageSize,new DefaultResponseListener<BaseResponse<List<News>>>(){
+        homeService.getHomeNews(AppConfig.getUid(AppManager.getPreferences()), pageNo, pageSize, new DefaultResponseListener<BaseResponse<List<News>>>() {
 
             @Override
             public void onSuccess(BaseResponse<List<News>> response) {
-                Log.e(TAG,response.toString());
+                Log.e(TAG, response.toString());
                 List<News> news = response.getResult();
-                for(News item:news){
-                    Log.e(TAG,item.toString());
+                for (News item : news) {
+                    Log.e(TAG, item.toString());
                 }
             }
 
@@ -88,5 +95,23 @@ public class FragmentA extends BaseFragment {
         });
     }
 
+
+    public class LocalImageHolderView implements Holder<Ad> {
+        private NetworkImageView imageView;
+
+        @Override
+        public View createView(Context context) {
+            imageView = new NetworkImageView(context);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            return imageView;
+        }
+
+        @Override
+        public void UpdateUI(Context context, final int position, Ad ad) {
+            Log.e(TAG,"position:"+position);
+            String imgUrl = RequestURL.SERVER + ad.getPicPath();
+            imageView.setImageUrl(imgUrl, AppManager.getImageLoader());
+        }
+    }
 
 }
