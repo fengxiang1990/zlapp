e6ee2bac40959e4b8f716d53c0703e3c8ca2e0c9
303153041@qq.com <fengxiang1990529>
2016-02-19 18:17:03 +0800

    首页

--- app/src/main/java/com.zl.app/adapter/NewsAdapter.java
+++ app/src/main/java/com.zl.app/adapter/NewsAdapter.java
@@ -1,16 +1,21 @@
 package com.zl.app.adapter;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.zl.app.BaseFragment;
 import com.zl.app.R;
 import com.zl.app.activity.news.NewsDetailActivity_;
 import com.zl.app.data.news.model.YyMobileNews;
+import com.zl.app.fragment.FragmentA;
+import com.zl.app.fragment.FragmentB;
+import com.zl.app.util.RequestURL;
 
 import java.util.List;
 
@@ -21,7 +26,8 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
     List<YyMobileNews> data;
     BaseFragment fragment;
-    public NewsAdapter(BaseFragment fragment,List<YyMobileNews> data) {
+
+    public NewsAdapter(BaseFragment fragment, List<YyMobileNews> data) {
         this.fragment = fragment;
         this.data = data;
     }
@@ -36,16 +42,27 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(NewsAdapter.ViewHolder holder, int position) {
         final YyMobileNews news = data.get(position);
-        holder.mTextView.setText(news.getHeadline());
-        holder.mTextView.setClickable(true);
-        holder.mTextView.setOnClickListener(new View.OnClickListener() {
+        Uri uri = Uri.parse(RequestURL.SERVER + news.getPicPath());
+        holder.draweeView.setImageURI(uri);
+        holder.text_title.setText(news.getHeadline());
+        holder.text_zhaiyao.setText(news.getZhaiyao());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(fragment.getActivity(), NewsDetailActivity_.class);
-                intent.putExtra("NEWS_ID",news.getUrl());
+                intent.putExtra("NEWS_ID", news.getUrl());
                 fragment.getActivity().startActivity(intent);
             }
         });
+        //滚动到底部自动加载
+        if(position == data.size()-1){
+           if(fragment instanceof FragmentA) {
+               FragmentA fragmentA = (FragmentA) fragment;
+               fragmentA.isLoadMore = true;
+               fragmentA.pageNo+=1;
+               fragmentA.loadNews();
+           }
+        }
     }
 
     @Override
@@ -58,11 +75,15 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
     //自定义的ViewHolder，持有每个Item的的所有界面元素
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView mTextView;
+        public TextView text_title;
+        public TextView text_zhaiyao;
+        public SimpleDraweeView draweeView;
 
         public ViewHolder(View view) {
             super(view);
-            mTextView = (TextView) view.findViewById(R.id.text_news_title);
+            text_title = (TextView) view.findViewById(R.id.text_news_title);
+            text_zhaiyao = (TextView) view.findViewById(R.id.text_news_zhaiyao);
+            draweeView = (SimpleDraweeView) view.findViewById(R.id.pic_news);
         }
     }
 }
--- app/src/main/java/com.zl.app/fragment/FragmentA.java
+++ app/src/main/java/com.zl.app/fragment/FragmentA.java
@@ -1,6 +1,7 @@
 package com.zl.app.fragment;
 
 import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -38,35 +39,57 @@ import java.util.List;
  */
 
 @EFragment(R.layout.fragment_a)
-public class FragmentA extends BaseFragment {
+public class FragmentA extends BaseFragment implements SwipeRefreshLayout.OnRefreshListener {
 
     private final String TAG = "FragmentA";
 
     @ViewById(R.id.recyclerView)
     RecyclerView recyclerView;
 
+    @ViewById(R.id.id_swipe_ly)
+    SwipeRefreshLayout swipeRefreshLayout;
+
     LinearLayoutManager mlinearLayoutManager;
     List<YyMobileNews> newsList;
     NewsAdapter newsAdapter;
     ConvenientBanner convenientBanner;
-    List<View> views;
-    int pageNo = 1;
-    int pageSize = 100;
+    public int pageNo = 1;
+    public int pageSize = 10;
 
     HomeService homeService;
 
+    public boolean isLoadMore = false;
+
     @AfterViews
     void afterViews() {
+        swipeRefreshLayout.setOnRefreshListener(this);
         convenientBanner = (ConvenientBanner) getView().findViewById(R.id.convenientBanner);
         mlinearLayoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(mlinearLayoutManager);
         //设置每个item 高度固定
         recyclerView.setHasFixedSize(true);
         newsList = new ArrayList<YyMobileNews>();
-        newsAdapter = new NewsAdapter(this,newsList);
+        newsAdapter = new NewsAdapter(this, newsList);
         recyclerView.setAdapter(newsAdapter);
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                int topRowVerticalPosition =
+                        (recyclerView == null || recyclerView.getChildCount() == 0) ? 0 : recyclerView.getChildAt(0).getTop();
+                swipeRefreshLayout.setEnabled(topRowVerticalPosition >= 0);
 
+            }
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+        });
         homeService = new HomeServiceImpl();
+        loadData();
+    }
+
+    public void loadData() {
         homeService.getHomeAds(AppConfig.getUid(AppManager.getPreferences()), new DefaultResponseListener<BaseResponse<List<YyMobileAdvt>>>() {
             @Override
             public void onSuccess(BaseResponse<List<YyMobileAdvt>> response) {
@@ -91,13 +114,25 @@ public class FragmentA extends BaseFragment {
 
             }
         });
+        loadNews();
+
+    }
+
+    public void loadNews() {
+        if (isLoadMore) {
+            swipeRefreshLayout.setRefreshing(true);
+        }
 
         homeService.getHomeNews(AppConfig.getUid(AppManager.getPreferences()), pageNo, pageSize, new DefaultResponseListener<BaseResponse<List<YyMobileNews>>>() {
 
             @Override
             public void onSuccess(BaseResponse<List<YyMobileNews>> response) {
+                swipeRefreshLayout.setRefreshing(false);
                 Log.e(TAG, response.toString());
                 List<YyMobileNews> news = response.getResult();
+                if (!isLoadMore) {
+                    newsList.clear();
+                }
                 newsList.addAll(news);
                 newsAdapter.notifyDataSetChanged();
             }
@@ -107,6 +142,13 @@ public class FragmentA extends BaseFragment {
 
             }
         });
+    }
+
+    @Override
+    public void onRefresh() {
+        pageNo = 1;
+        isLoadMore = false;
+        loadData();
     }
 
 
