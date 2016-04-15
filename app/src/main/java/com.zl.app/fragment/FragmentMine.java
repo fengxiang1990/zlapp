7fd91e3434d073a98270b5cb7d034656f38e031b
303153041@qq.com <fengxiang1990529>
2016-04-15 15:44:12 +0800

    我的主页

--- app/src/main/java/com.zl.app/fragment/FragmentMine.java
+++ app/src/main/java/com.zl.app/fragment/FragmentMine.java
@@ -1,9 +1,20 @@
 package com.zl.app.fragment;
 
+import android.net.Uri;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.zl.app.BaseFragment;
 import com.zl.app.R;
+import com.zl.app.util.AppConfig;
+import com.zl.app.util.AppManager;
+import com.zl.app.util.RequestURL;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.App;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
 
 /**
  * 我的
@@ -12,4 +23,25 @@ import org.androidannotations.annotations.EFragment;
 @EFragment(R.layout.fragment_mine)
 public class FragmentMine extends BaseFragment{
 
+    String tag = "FragmentMine";
+
+    @ViewById(R.id.simpleDraweeView)
+    SimpleDraweeView simpleDraweeView;
+
+    @ViewById(R.id.text_name)
+    TextView text_name;
+
+    String uid;
+
+    @AfterViews
+    void afterViews() {
+        uid = AppConfig.getUid(AppManager.getPreferences());
+        String picPath = AppConfig.getUserHeadImg(AppManager.getPreferences());
+        picPath = RequestURL.SERVER + picPath;
+        Log.e(tag, picPath);
+        Uri uri = Uri.parse(picPath);
+        simpleDraweeView.setImageURI(uri);
+        String username = AppManager.getPreferences().getString(AppConfig.USER_NAME, "");
+        text_name.setText(username);
+    }
 }
