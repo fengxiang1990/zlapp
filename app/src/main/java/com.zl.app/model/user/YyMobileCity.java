5ed55ba53f0958ab178d50142ddcbe95e0278b3e
303153041@qq.com <fengxiang1990529>
2016-04-25 12:56:54 +0800

    学生课程动态

---
+++ app/src/main/java/com.zl.app/model/user/YyMobileCity.java
@@ -0,0 +1,33 @@
+package com.zl.app.model.user;
+
+
+public class YyMobileCity {
+    private Integer cityId;
+    private Integer parentId;
+    private String name;
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
