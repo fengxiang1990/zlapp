f0714c15dc0fbe4a287a4fcd1eaac0d15e629f92
fxa <303153041@qq.com>
2016-07-14 23:39:37 +0800

    孩子二维码  系统反馈

---
+++ app/src/main/java/com.zl.app/data/InitiationService.java
@@ -0,0 +1,39 @@
+package com.zl.app.data;
+
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.google.gson.reflect.TypeToken;
+import com.zl.app.data.model.initiation.YyMobileZiluNews;
+import com.zl.app.util.AppManager;
+import com.zl.app.util.GsonUtil;
+import com.zl.app.util.RequestURL;
+import com.zl.app.util.net.BaseResponse;
+import com.zl.app.util.net.DefaultResponseListener;
+import com.zl.app.util.net.GsonRequest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by fxa on 2016/7/14.
+ */
+
+public class InitiationService {
+
+    String api_news_list = RequestURL.SERVER + "mobileZiluNews/list.html";
+
+    public void getNewsList(int pageNo, int pageSize, String uid, DefaultResponseListener<BaseResponse<List<YyMobileZiluNews>>> listener) {
+        Map<String, String> map = new HashMap<>();
+        map.put("pageNo", pageNo + "");
+        map.put("pageSize", pageSize + "");
+        map.put("uid", uid);
+        Log.e("getNewsList", GsonUtil.gson.toJson(map));
+        GsonRequest request = new GsonRequest(Request.Method.POST, api_news_list, map, null,
+                new TypeToken<BaseResponse<List<YyMobileZiluNews>>>() {
+                },
+                listener, listener);
+        AppManager.getRequestQueue().add(request);
+    }
+}
--- app/src/main/java/com.zl.app/fragment/FragmentInitiation.java
+++ app/src/main/java/com.zl.app/fragment/FragmentInitiation.java
@@ -1,5 +1,7 @@
 package com.zl.app.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -8,14 +10,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.android.volley.VolleyError;
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.zl.app.BaseFragment;
 import com.zl.app.R;
+import com.zl.app.activity.org.WebDetailActivity;
+import com.zl.app.data.InitiationService;
+import com.zl.app.data.model.initiation.YyMobileZiluNews;
+import com.zl.app.util.AppConfig;
+import com.zl.app.util.AppManager;
+import com.zl.app.util.ToastUtil;
+import com.zl.app.util.net.BaseResponse;
+import com.zl.app.util.net.DefaultResponseListener;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.SystemService;
 import org.androidannotations.annotations.ViewById;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.finalteam.loadingviewfinal.HeaderAndFooterRecyclerViewAdapter;
 import cn.finalteam.loadingviewfinal.OnLoadMoreListener;
 import cn.finalteam.loadingviewfinal.RecyclerViewFinal;
 import cn.finalteam.loadingviewfinal.SwipeRefreshLayoutFinal;
@@ -49,18 +65,72 @@ public class FragmentInitiation extends BaseFragment implements SwipeRefreshLayo
         layoutManager = new LinearLayoutManager(getActivity());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasLoadMore(true);
+//        recyclerView.addItemDecoration(new RecycleViewDivider(getActivity(), LinearLayoutManager.VERTICAL
+//        ,10,getResources().getColor(R.color.black)));
         adapter = new MyAdapter();
         recyclerView.setAdapter(adapter);
+        uid = AppConfig.getUid(AppManager.getPreferences());
+        initiationService = new InitiationService();
+        loadData();
+        recyclerView.setOnItemClickListener(new HeaderAndFooterRecyclerViewAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(RecyclerView.ViewHolder holder, int position) {
+                YyMobileZiluNews ziluNews = data.get(position);
+                Intent intent = new Intent(getActivity(), WebDetailActivity.class);
+                intent.putExtra("url", ziluNews.getUrl());
+                intent.putExtra("title", ziluNews.getHeadline());
+                startActivity(intent);
+            }
+        });
     }
+
+    int pageNo = 1;
+    int pageSize = 20;
+    InitiationService initiationService;
+    String uid;
+    boolean isLoadMore = false;
+    List<YyMobileZiluNews> data = new ArrayList<>();
 
     @Override
     public void onRefresh() {
-
+        pageNo = 1;
+        isLoadMore = false;
+        loadData();
     }
 
     @Override
     public void loadMore() {
+        pageNo += 1;
+        isLoadMore = true;
+        loadData();
+    }
 
+    void loadData() {
+        swipe.setRefreshing(true);
+        initiationService.getNewsList(pageNo, pageSize, uid, new DefaultResponseListener<BaseResponse<List<YyMobileZiluNews>>>() {
+            @Override
+            public void onSuccess(BaseResponse<List<YyMobileZiluNews>> response) {
+                swipe.onRefreshComplete();
+                recyclerView.onLoadMoreComplete();
+                if (response.getStatus().equals(AppConfig.HTTP_OK)) {
+                    if (!isLoadMore) {
+                        data.clear();
+                    }
+                    data.addAll(response.getResult());
+                    adapter.notifyDataSetChanged();
+                } else {
+                    ToastUtil.show(getActivity(), response.getMessage());
+                }
+
+            }
+
+            @Override
+            public void onError(VolleyError error) {
+                swipe.onRefreshComplete();
+                recyclerView.onLoadMoreComplete();
+            }
+        });
     }
 
     class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolde> {
@@ -68,30 +138,38 @@ public class FragmentInitiation extends BaseFragment implements SwipeRefreshLayo
 
         @Override
         public MyViewHolde onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = inflate.inflate(R.layout.item_initiation, null);
+            View view = inflate.inflate(R.layout.item_initiation, parent, false);
             MyViewHolde holder = new MyViewHolde(view);
             return holder;
         }
 
         @Override
         public void onBindViewHolder(MyViewHolde holder, int position) {
-            holder.text_title.setText("title" + position);
-            holder.text_content.setText("content" + position);
+            YyMobileZiluNews ziluNews = data.get(position);
+            holder.text_title.setText(ziluNews.getHeadline());
+            holder.text_content.setText(ziluNews.getZhaiyao());
+            holder.text_see_count.setText(ziluNews.getClickNo() + "");
+            holder.text_time.setText(ziluNews.getCreateDateFormat());
+            holder.simpleDraweeView.setImageURI(Uri.parse(ziluNews.getPicPath()));
         }
 
         @Override
         public int getItemCount() {
-            return 20;
+            return data.size();
         }
 
         class MyViewHolde extends RecyclerView.ViewHolder {
 
-            TextView text_title, text_content;
+            TextView text_title, text_content, text_see_count, text_time;
+            SimpleDraweeView simpleDraweeView;
 
             public MyViewHolde(View itemView) {
                 super(itemView);
                 text_title = (TextView) itemView.findViewById(R.id.text_title);
                 text_content = (TextView) itemView.findViewById(R.id.text_content);
+                text_see_count = (TextView) itemView.findViewById(R.id.text_see_count);
+                text_time = (TextView) itemView.findViewById(R.id.text_time);
+                simpleDraweeView = (SimpleDraweeView) itemView.findViewById(R.id.simpleDraweeView);
             }
         }
     }
